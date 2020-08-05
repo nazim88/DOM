2d7030aa44e146f02f73a3a3e53f87e41c0b16c3
nazim88 <68301679+nazim88@users.noreply.github.com>
2020-08-05 15:17:36 +0100

    Add files via upload

--- js/Panier.js
+++ js/Panier.js
@@ -4,8 +4,8 @@ let Deletes = document.querySelectorAll(".remove");
 let likes = document.querySelectorAll(".like");
 let adds = document.querySelectorAll("div a.add");
 let removes = document.querySelectorAll("div a.rem");
-let price = document.querySelectorAll(".price")
-let quantite = document.querySelectorAll(".Quantity")
+let prices = document.querySelectorAll(".price")
+let quantites = document.querySelectorAll(".Quantity")
 let totals = document.querySelectorAll(".total")
 let sums = document.querySelector(".global")
 /**********************add quantity  function*****************/
@@ -15,6 +15,15 @@ for (add = 0; add < adds.length; add++) {
   function count(event) {
     n++;
     this.nextElementSibling.value = n;
+    let total = prices[0].value * quantites[0].value
+    totals[0].value = total
+  
+    let total2 = prices[1].value * quantites[1].value
+    totals[1].value = total2
+  
+    let total3 = prices[2].value * quantites[2].value
+    totals[2].value = total3
+    sums.value = (total + total2 + total3).toFixed(2);
   }
   for (remove = 0; remove < removes.length; remove++) {
     removes[remove].addEventListener("click", countm);
@@ -23,6 +32,15 @@ for (add = 0; add < adds.length; add++) {
         n2 = n--;
       }
       this.previousElementSibling.value = n2;
+      let total = prices[0].value * quantites[0].value
+      totals[0].value = total
+    
+      let total2 = prices[1].value * quantites[1].value
+      totals[1].value = total2
+    
+      let total3 = prices[2].value * quantites[2].value
+      totals[2].value = total3
+      sums.value = (total + total2 + total3).toFixed(2);
     }
   }
 }
@@ -37,26 +55,14 @@ function clicLike() {
 for (Delete = 0; Delete < Deletes.length; Delete++) {
   Deletes[Delete].addEventListener("click", deleteClick);
 }
+
 function deleteClick() {
   let res = confirm("Are you sure you want to delete ?");
     if(res){
-      this.parentElement.parentElement.remove();
-      
+      this.parentElement.parentElement.remove()
+       sums.value = 0
     }
   
-
 }
 /************************** see total price function*****************/
 
-function calcul() {
-  let total = price[0].value * quantite[0].value
-  totals[0].value = total
-
-  let total2 = price[1].value * quantite[1].value
-  totals[1].value = total2
-
-  let total3 = price[2].value * quantite[2].value
-  totals[2].value = total3
-
-  sums.value = (total + total2 + total3).toFixed(2);
-}
